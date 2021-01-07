5707136212f11d22afd2ed53eabff39c54788294
MadMakZa <immakza@gmail.com>
2021-01-07 18:06:04 +0300

    test reduce subscribe

--- app/src/main/java/com/example/rxjavatutorialjava/RxDemo.java
+++ app/src/main/java/com/example/rxjavatutorialjava/RxDemo.java
@@ -7,10 +7,31 @@ import io.reactivex.Observable;
 
 public class RxDemo {
 
+
+
     public static void testRx(){
-        //создание переменной
-        Observable observable = Observable.just(Log.d("log","Hello Makza"));
-        //подписываемся к ней и печатаем текст на который подписались
-        observable.subscribe();
+
+        String name = "Maxim";
+        String lastName = "Fon";
+        String rang = "General";
+
+//        /*
+//        обычно так не делают
+//         */
+//        //создание переменной
+//        Observable observable = Observable.just(Log.d("log","Hello Makza"),(Log.d("log","Are you kidding me?")));
+//        //подписываемся к ней и печатаем текст на который подписались
+//        observable.subscribe();
+
+        /*
+        А делают так
+         */
+        Observable.just(name ,lastName, rang)
+                .reduce((a,b) -> a+b)
+                .subscribe(RxDemo::printer);
+    }
+
+    public static void printer(String string){
+        Log.d("log", string);
     }
 }
