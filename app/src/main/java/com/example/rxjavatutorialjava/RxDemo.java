5fb53b33509c9d2c6805a011b5795b05acb5f6de
MadMakZa <immakza@gmail.com>
2021-01-07 17:41:53 +0300

    test subscribe method Just

--- app/src/main/java/com/example/rxjavatutorialjava/RxDemo.java
+++ app/src/main/java/com/example/rxjavatutorialjava/RxDemo.java
@@ -1,5 +1,16 @@
 package com.example.rxjavatutorialjava;
 
+
+import android.util.Log;
+
+import io.reactivex.Observable;
+
 public class RxDemo {
-    
+
+    public static void testRx(){
+        //создание переменной
+        Observable observable = Observable.just(Log.d("log","Hello Makza"));
+        //подписываемся к ней и печатаем текст на который подписались
+        observable.subscribe();
+    }
 }
