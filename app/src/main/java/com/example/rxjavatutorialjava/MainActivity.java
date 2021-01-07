d3321599e197ac021710bd157de6d7283a344233
MadMakZa <immakza@gmail.com>
2021-01-08 01:30:04 +0300

    practic with AlertDialog

--- app/src/main/java/com/example/rxjavatutorialjava/MainActivity.java
+++ app/src/main/java/com/example/rxjavatutorialjava/MainActivity.java
@@ -1,18 +1,52 @@
 package com.example.rxjavatutorialjava;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
 
 import static com.example.rxjavatutorialjava.RxDemo.testRx;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private EditText mEditText;
+    private PublishSubject<String> mTextChangedSubject;
+
+    private void init(){
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mEditText = findViewById(R.id.editText);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        showDialog("First question", "Yes", "No");
 
-        testRx();
+    }
+
+    Observable<Integer> showDialog(String title, String positive, String negative){
+
+        PublishSubject publishSubject = PublishSubject.create();
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this)
+                .setTitle(title)
+                //передаем индекс нажатой кнопки
+                .setPositiveButton(positive, (view, index) -> publishSubject.onNext(index))
+                .setNegativeButton(negative,(view, index) -> publishSubject.onNext(index));
+
+        return publishSubject
+                .hide()
+                .doOnSubscribe(disposable -> dialog.show());
+
+
     }
 }
